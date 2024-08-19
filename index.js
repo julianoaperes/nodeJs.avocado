f65bc595c31f32e887ca0855bb690fe7d25a7a67
Juliano Peres <juliano.a.peres@gmail.com>
2024-08-19 18:15:25 -0300

    back button adjust

--- index.js
+++ index.js
@@ -25,10 +25,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req,res) => {
-   const pathName = req.url;
-  
+
+    const {query, pathname} =  url.parse(req.url, true);
+     
    // Overview page
-   if (pathName === '/' || pathName === '/overview') {
+   if (pathname === '/' || pathname === '/overview') {
      res.writeHead(200, { 'Content-type':'text/html'});
 
      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); 
@@ -36,16 +37,19 @@ const server = http.createServer((req,res) => {
      res.end(output);
 
      // Product page
-    } else if (pathName === '/product') {
-      res.end('Here are the PRODUCTS.');  
+     } else if (pathname === '/product') {
+      res.writeHead(200, { 'Content-type':'text/html'});
+      const product = dataObj[query.id];
+      const output = replaceTemplate(tempProduct, product);
+      res.end(output);  
 
       //API
-    } else if (pathName === '/api') {        
+     } else if (pathname === '/api') {        
       res.writeHead(200, { 'Content-type': 'application/json'});
       res.end(data);
 
      // Not Found
-    } else {
+     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-word',
