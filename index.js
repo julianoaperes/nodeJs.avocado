b9b4a46b143fcb89dd25866915ef68557710f031
Juliano Peres <juliano.a.peres@gmail.com>
2024-08-20 07:03:52 -0300

    Warning: in the working copy of 'package.json', LF will be replaced by CRLF the next time Git touches it

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -11,6 +13,8 @@ const tempProduct  = fs.readFileSync(`${__dirname}/templates/template-product.ht
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+console.log(slugify('Fresh Avocados', { lower: true})); 
 
 const server = http.createServer((req,res) => {
 
